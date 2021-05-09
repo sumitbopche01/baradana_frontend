206ebbf5f622adc08eed5d5b019b0d2b00e7a0ae
Sumit Bopche <sumit@freightbro.com>
2021-05-09 15:30:54 +0530

    login page route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./pages/home";
+import Login from "./pages/loginPage";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" component={Home} exact />
+        <Route path="/login" component={Login} exact />
       </Switch>
     </Router>
   );
